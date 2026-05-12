b0786f8bea70c1112ca75b2e97a43b54a3278e94
markvincentmillaminaazupardo <markvincentmillamina.azupardo25@bicol-u.edu.ph>
2026-05-12 23:06:16 +0800

    portfolio1.py

--- portfolio1.py
+++ portfolio1.py
@@ -1,15 +1,15 @@
-# Portfolio 1: My Daily Allowance Tracker
+#Portfolio 1: My Daily Allowance Tracker
 
-# **Description:**
-# A simple program I made to show how my allowance is deducted by my expenses.
+#**Description:**
+#A simple program I made to show how my allowance is deducted by my expenses.
 
-# I put my numbers directly into the variables
+#I put my numbers directly into the variables
 my_allowance = 1000
 my_expenses = 890
 
 cash_left = my_allowance - my_expenses
 
-# Display the results to show it worked
+#Display the results to show it worked
 print("My total allowance was:", my_allowance)
 print("I spent exactly:", my_expenses)
 print("After deducting, my remaining cash is:", cash_left)
